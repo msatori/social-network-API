aff3d0749bf0f7195ba237a77931daecb295bf05
Maya Santiago <69778090+msatori@users.noreply.github.com>
2021-01-29 09:49:37 -0600

    began user controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,45 @@
+const { User } = require('../models');
+
+const userController = {
+  // get all users
+  getAllUsers(req, res) {
+    User.find()
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // get one user by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({   
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+
+  // createUser
+  createUser({ body }, res) {
+    User.create(body)
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
+  },
+
+ 
+  // delete pizza
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => res.json(err));
+  }
+};
+
+module.exports = userController;
