3a90f59956b96b2b665f0ef8c6ee963ff4f70101
Maya Santiago <mayasantiago@Mayas-MacBook-Pro.local>
2021-02-05 15:22:06 -0600

    reactions not working

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -56,21 +56,20 @@ const thoughtController = {
 
     //edit thought 
     editThought(req, res) {
-        Thought.findOneAndUpdate(
-            { _id: req.params.thoughtId },
-            { $set: req.body },
-            { runValidators: true, new: true }
-        )
-            .then((dbThoughtData) => {
-                if (!dbThoughtData) {
-                    return res.status(404).json({ message: 'No thought with this id exists' })
-                }
-                res.json(dbThoughtData);
-            })
-            .catch((err) => {
-                console.log(err);
-                res.status(500).json(err)
-            });
+        Thought.findOneAndUpdate({ _id: req.params.thoughtId },
+             { $set: req.body },
+             { runValidators: true, new: true })
+        .then((dbThoughtData) => {
+          if (!dbThoughtData) {
+            return res.status(404).json({ message: 'No thought with this id!' });
+          }
+          res.json(dbThoughtData);
+        })
+        .catch((err) => {
+          console.log(err);
+          res.status(500).json(err);
+        });
+  
     },
 
     //delete thought 
